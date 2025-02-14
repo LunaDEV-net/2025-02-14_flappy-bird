858c54c70028d4d0e594503df2bdde1584885e29
luna <93258461+LunaDEV-net@users.noreply.github.com>
2025-02-14 13:04:38 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
+function gen_pip () {
+	
+}
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (bird_cur > 0) {
         bird_cur += -1
     }
 })
 let bird_cur = 0
+let dis_to_last = 0
 bird_cur = 2
 let bird_old = bird_cur
 loops.everyInterval(1000, function () {
+    if (1 == randint(1, 5 - dis_to_last)) {
+    	
+    }
+})
+loops.everyInterval(500, function () {
     if (bird_cur < 4) {
         bird_cur += 1
     }
